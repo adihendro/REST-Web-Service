ba4c0788d62507cd1552ecc5e2224ed6982537ce
adihendro <adi21hendro@gmail.com>
2020-12-14 17:51:50 +0700

    fix: add port in index

--- index.js
+++ index.js
@@ -5,28 +5,50 @@ app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
-app.listen(3000, () => {
-    console.log(`Server started on port`);
+var port = process.env.PORT || 3000
+
+app.listen(port, () => {
+    console.log(`Server started on port ` + port);
 });
 
 
-const { Pool } = require('pg');
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
+// const { Pool } = require('pg');
+// const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: {
+//     rejectUnauthorized: false
+//   }
+// });
 
-app.get('/db', async (req, res) => {
-  try {
-    const client = await pool.connect();
-    const result = await client.query('SELECT * FROM favourites');
-    const results = { 'results': (result) ? result.rows : null};
-    res.render('pages/db', results );
-    client.release();
-  } catch (err) {
-    console.error(err);
-    res.send("Error " + err);
-  }
-})
+// app.get('/db', async (req, res) => {
+//   try {
+//     const client = await pool.connect();
+//     const result = await client.query('SELECT * FROM favourites');
+//     const results = { 'results': (result) ? result.rows : null};
+//     res.render('pages/db', results );
+//     client.release();
+//   } catch (err) {
+//     console.error(err);
+//     res.send("Error " + err);
+//   }
+// })
+
+
+// const { Client } = require('pg');
+
+// const client = new Client({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: {
+//     rejectUnauthorized: false
+//   }
+// });
+
+// client.connect();
+
+// client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+//   if (err) throw err;
+//   for (let row of res.rows) {
+//     console.log(JSON.stringify(row));
+//   }
+//   client.end();
+// });
