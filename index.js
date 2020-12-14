46f516e085e390804ef46b00b361c966bb7bc822
adihendro <adi21hendro@gmail.com>
2020-12-14 19:15:42 +0700

    chore: add postgres database

--- index.js
+++ index.js
@@ -1,54 +1,46 @@
 const express = require('express')
-const app = express()
+const router = express.Router();
+const app = express();
  
 app.get('/', function (req, res) {
   res.send('Hello World')
+  // res.render("index")
 })
 
-var port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log(`Server started on port ` + port);
 });
 
-
-// const { Pool } = require('pg');
-// const pool = new Pool({
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: {
-//     rejectUnauthorized: false
-//   }
+// // Main Route
+// router.get('/', function(req,res){
+//     // res.render("index");
+//     res.send('Hello World')
 // });
 
-// app.get('/db', async (req, res) => {
-//   try {
-//     const client = await pool.connect();
-//     const result = await client.query('SELECT * FROM favourites');
-//     const results = { 'results': (result) ? result.rows : null};
-//     res.render('pages/db', results );
-//     client.release();
-//   } catch (err) {
-//     console.error(err);
-//     res.send("Error " + err);
-//   }
-// })
 
 
-// const { Client } = require('pg');
+const DATABASE_URL = 'postgres://tpkastkkjlgyni:a433516bb6d8785e4c77aa6f0c61b9e6f596e6f69fb21f03124411ae067fd8f1@ec2-52-203-182-92.compute-1.amazonaws.com:5432/d71f3sln708rq0';
 
-// const client = new Client({
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: {
-//     rejectUnauthorized: false
-//   }
-// });
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: 'postgres://tpkastkkjlgyni:a433516bb6d8785e4c77aa6f0c61b9e6f596e6f69fb21f03124411ae067fd8f1@ec2-52-203-182-92.compute-1.amazonaws.com:5432/d71f3sln708rq0',
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
-// client.connect();
-
-// client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-//   if (err) throw err;
-//   for (let row of res.rows) {
-//     console.log(JSON.stringify(row));
-//   }
-//   client.end();
-// });
+app.get('/db', async (req, res) => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query('SELECT * FROM favourites');
+    const results = { 'results': (result) ? result.rows : null};
+    res.send(results);
+    // res.render('pages/db', results );
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+})
