60207f56896ad88a5d003bb9309c5782d6f5c241
adihendro <adi21hendro@gmail.com>
2020-12-14 16:28:06 +0700

    feat: add postgres database

--- src/index.js
+++ src/index.js
@@ -8,3 +8,25 @@ app.get('/', function (req, res) {
 app.listen(3000, () => {
     console.log(`Server started on port`);
 });
+
+
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+app.get('/db', async (req, res) => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query('SELECT * FROM favourites');
+    const results = { 'results': (result) ? result.rows : null};
+    res.render('pages/db', results );
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+})
