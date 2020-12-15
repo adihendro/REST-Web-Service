62a08c21acd8effba4d9c66477850e856fc615ee
adihendro <adi21hendro@gmail.com>
2020-12-15 16:26:32 +0700

    fix: separate backend frontend

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 //requiring modules
 const express = require('express');
+// const bodyParser = require('body-parser');
+const cookieSession = require('cookie-session')
 const app = express();
+
+//Oauth
+const passport = require('passport');
+app.use(passport.initialize());
+app.use(passport.session());
+require('./passport');
+
+
+// app.use(bodyParser.urlencoded({extended:false}));
+// app.use(bodyParser.json());
+
 
 //requiring models
 const favouritesRoutes = require('./src/routes/favourites');
@@ -9,12 +22,53 @@ const gamesRoutes = require('./src/routes/games');
 //Routes
 app.use("/favourites", favouritesRoutes);
 app.use("/games", gamesRoutes);
+app.use("/games", gamesRoutes);
 
 
-// app.get('/',  (req, res) => {
-//   res.send('Hello World')
-//   // res.render("index")
-// });
+app.get('/', (req,res) => {
+  res.send('Hello World, you are not logged in')
+});
+
+
+
+
+const isLoggedIn = (req, res, next) => {
+  if(name){
+    next();
+  } else{
+    res.sendStatus(401);
+  }
+}
+
+app.use(cookieSession({
+  name: 'video-games-session',
+  keys: ['key1', 'key2']
+}))
+
+
+var name=null;
+app.get('/user', isLoggedIn, (req, res) => {
+  // console.log("asfda "+name);
+
+  res.send(`Welcome ${name}!`)
+});
+
+app.get('/login', passport.authenticate('google', { scope: ['profile', 'email'] }));
+
+app.get('/login/callback', passport.authenticate('google', { failureRedirect: '/' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/user');
+    name=req.user.displayName;
+  });
+
+app.get('/logout', (req, res) => {
+  req.session = null;
+  name=null;
+  req.logout();
+  res.redirect('/');
+});
+
 
 
 // Port
---
+++ passport.js
@@ -0,0 +1,26 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+passport.serializeUser(function(user, done) {
+    done(null, user);
+  });
+  
+passport.deserializeUser(function(user, done) {
+    // User.findById(id, function(err, user) {
+    done(null, user);
+    // });
+});
+
+
+passport.use(new GoogleStrategy({
+    clientID: "947828860166-g72vv0d5c7purcel6h3gc1shehm7sul4.apps.googleusercontent.com",
+    clientSecret: "m7l8Cr4_JycUAijpkeNumWek",
+    callbackURL: "http://localhost:3000/login/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    // User.findOrCreate({ googleId: profile.id }, function (err, user) {
+    // console.log("sdfa "+profile.displayName);;  
+    return done(null, profile);
+    // });
+  }
+));
